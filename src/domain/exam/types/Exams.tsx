233b015a73972bf2a84ad250986f3c2228191f2e
Paul Kirchhoff <paulkirchhoffbln@gmail.com>
2024-04-13 22:38:09 +0200

    add questions

--- src/domain/exam/types/Exams.tsx
+++ src/domain/exam/types/Exams.tsx
@@ -5,7 +5,7 @@ export interface Exam {
   id: string,
   title: string,
   description: string,
-  time: 25,
+  time: number,
   questions: Question[],
   passingScore: number
   path: string,
@@ -25,100 +25,352 @@ export const Exams: Exam[] = [
     questions: [
       {
         id: '1',
-        question: 'Test',
+        question: 'What is the purpose of this hook?',
+        code: 'useEffect(() => {\n' +
+            ' document. title = `You clicked ${count} times`;\n' +
+            '3, [count]);)',
         type: QuestionType.MULTIPLE_CHOICE,
         points: 1,
         answers: [
           {
             id: 'A',
             isCorrect: false,
-            answer: 'qwertz',
+            answer: 'There is a syntax error, this hook won\'t work',
             isSelected: false
           },
           {
             id: 'B',
-            isCorrect: false,
-            answer: 'ztrewq',
+            isCorrect: true,
+            answer: 'Change the page title every time the count state variable is updated',
             isSelected: false
           },
           {
             id: 'C',
             isCorrect: false,
-            answer: 'yxcvb',
+            answer: 'Change the page title every time any state variable is updated',
             isSelected: false
           },
           {
             id: 'D',
-            isCorrect: true,
-            answer: 'bvcxy',
+            isCorrect: false,
+            answer: 'Change the page title every time props gets updated',
             isSelected: false
           },
         ]
       },
       {
         id: '2',
-        question: 'Test 2',
+        question: 'What name is given to form elements that have their internal state synchronized with the state of their parent React component?',
         type: QuestionType.MULTIPLE_CHOICE,
         points: 1,
         answers: [
           {
             id: 'A',
             isCorrect: false,
-            answer: 'Test 1',
+            answer: 'Redux elements',
             isSelected: false
           },
           {
             id: 'B',
-            isCorrect: false,
-            answer: 'Test 2',
+            isCorrect: true,
+            answer: 'Controlled components',
             isSelected: false
           },
           {
             id: 'C',
             isCorrect: false,
-            answer: 'Test 3',
+            answer: 'Hook components',
             isSelected: false
           },
           {
             id: 'D',
-            isCorrect: true,
-            answer: 'Test 4',
+            isCorrect: false,
+            answer: 'Synchronized elements',
             isSelected: false
           },
         ]
       },
       {
         id: '3',
-        question: 'Test 3',
+        question: 'What would happen if we run the tick function 10 times consecutively?',
+        code: 'function tick {\n' +
+            'const element = (\n' +
+            '‹div>\n' +
+            '<h1>Hello, world!</h1>\n' +
+            '<h2>It is {new Date().toLocaleTimeString}.</h2>\n' +
+            '</ div>\n' +
+            ');\n' +
+            'ReactD0M. render(\n' +
+            'element, document.getElementById(\'root\')\n' +
+            ');\n' +
+            '}',
         type: QuestionType.MULTIPLE_CHOICE,
         points: 1,
         answers: [
           {
             id: 'A',
             isCorrect: false,
-            answer: 'Test 1',
+            answer: 'The div element would be displayed 10 times in the DOM.',
             isSelected: false
           },
           {
             id: 'B',
             isCorrect: false,
-            answer: 'Test 2',
+            answer: 'We would see an error in the browser console.',
             isSelected: false
           },
           {
             id: 'C',
             isCorrect: false,
-            answer: 'Test 3',
+            answer: 'The div element would be rendered by React and then refreshed 9 times consecutively in the browser DOM.',
+            isSelected: false
+          },
+          {
+            id: 'D',
+            isCorrect: false,
+            answer: 'The div element would be rendered by React and then refreshed 0 or 1 time in the browser DOM.',
+            isSelected: false
+          },
+        ]
+      },
+      {
+        id: '4',
+        question: 'What is the purpose of the className attribute in JSX?',
+        type: QuestionType.MULTIPLE_CHOICE,
+        points: 1,
+        answers: [
+          {
+            id: 'A',
+            isCorrect: false,
+            answer: 'It assigns a unique name to the component',
+            isSelected: false
+          },
+          {
+            id: 'B',
+            isCorrect: false,
+            answer: 'It specifies the component\'s name',
+            isSelected: false
+          },
+          {
+            id: 'C',
+            isCorrect: true,
+            answer: 'It sets the CSS class for an element',
+            isSelected: false
+          },
+          {
+            id: 'D',
+            isCorrect: false,
+            answer: 'It defines the component\'s ID',
+            isSelected: false
+          },
+        ]
+      },
+      {
+        id: '5',
+        question: 'Which React hook is used to manage component state?',
+        type: QuestionType.MULTIPLE_CHOICE,
+        points: 1,
+        answers: [
+          {
+            id: 'A',
+            isCorrect: false,
+            answer: 'useContext',
+            isSelected: false
+          },
+          {
+            id: 'B',
+            isCorrect: true,
+            answer: 'useState',
+            isSelected: false
+          },
+          {
+            id: 'C',
+            isCorrect: false,
+            answer: 'useStore',
+            isSelected: false
+          },
+          {
+            id: 'D',
+            isCorrect: false,
+            answer: 'useEffect',
+            isSelected: false
+          },
+        ]
+      },
+      {
+        id: '6',
+        question: 'What is the correct syntax to listen to a click event in a component and call a doSomething function when the click happens:',
+        type: QuestionType.MULTIPLE_CHOICE,
+        points: 1,
+        answers: [
+          {
+            id: 'A',
+            isCorrect: false,
+            answer: '(click)={doSomething}',
+            isSelected: false
+          },
+          {
+            id: 'B',
+            isCorrect: false,
+            answer: 'click={doSomething}',
+            isSelected: false
+          },
+          {
+            id: 'C',
+            isCorrect: true,
+            answer: 'onClick={doSomething}',
+            isSelected: false
+          },
+          {
+            id: 'D',
+            isCorrect: false,
+            answer: 'onClick={doSomething)}',
+            isSelected: false
+          },
+        ]
+      },
+      {
+        id: '7',
+        question: 'We want to update the price of an item by applying a discount to it. The price of that item is stored in the state of a component, and the discount value is passed to the component as a prop. What is the proper way to apply a discount to the price?',
+        type: QuestionType.MULTIPLE_CHOICE,
+        points: 1,
+        answers: [
+          {
+            id: 'A',
+            isCorrect: false,
+            answer: 'this.setState({\n' +
+                'price: this.state.price - this.props.discount\n' +
+                '3);',
+            isSelected: false
+          },
+          {
+            id: 'B',
+            isCorrect: false,
+            answer: 'this.setState((state) => ({\n' +
+                'price: state.price - this.props.discount}));',
+            isSelected: false
+          },
+          {
+            id: 'C',
+            isCorrect: false,
+            answer: 'this. setState(0 => ({\n' +
+                'price: this.state.price - this.props.discount\n' +
+                '3));',
+            isSelected: false
+          },
+          {
+            id: 'D',
+            isCorrect: false,
+            answer: 'this. setState((state, props) => (£\n' +
+                'price: state.price - props.discount\n' +
+                '3));',
+            isSelected: false
+          },
+        ]
+      },
+      {
+        id: '8',
+        question: 'Is there anything wrong with the following JSX syntax?',
+        code: '‹button onClick={extendDate} disabled={selection.isEmpty)}>\n' +
+            'Extend date for selection </button>',
+        type: QuestionType.MULTIPLE_CHOICE,
+        points: 1,
+        answers: [
+          {
+            id: 'A',
+            isCorrect: false,
+            answer: 'No, the code looks correct.',
+            isSelected: false
+          },
+          {
+            id: 'B',
+            isCorrect: false,
+            answer: 'Yes, some curly braces f} are missing.',
+            isSelected: false
+          },
+          {
+            id: 'C',
+            isCorrect: false,
+            answer: 'Yes, the click listener syntax should be (onClick).',
             isSelected: false
           },
           {
             id: 'D',
             isCorrect: true,
-            answer: 'Test 4',
+            answer: 'Yes, the onClick event listener is not going to work properly.',
+            isSelected: false
+          },
+        ]
+      },
+      {
+        id: '9',
+        question: 'In a functional component, when is the useEffect hook called for the first time?',
+        type: QuestionType.MULTIPLE_CHOICE,
+        points: 1,
+        answers: [
+          {
+            id: '1',
+            isCorrect: false,
+            answer: 'During component rendering',
+            isSelected: false
+          },
+          {
+            id: '2',
+            isCorrect: false,
+            answer: 'During component initialization',
+            isSelected: false
+          },
+          {
+            id: '3',
+            isCorrect: true,
+            answer: 'After the component has been inserted into the DOM',
+            isSelected: false
+          },
+          {
+            id: '4',
+            isCorrect: false,
+            answer: 'When the component\'s state changes',
+            isSelected: false
+          },
+        ]
+      },
+      {
+        id: '10',
+        question: 'What method does a class component have to define?',
+        type: QuestionType.MULTIPLE_CHOICE,
+        points: 1,
+        answers: [
+          {
+            id: 'A',
+            isCorrect: false,
+            answer: 'display',
+            isSelected: false
+          },
+          {
+            id: 'B',
+            isCorrect: false,
+            answer: 'refresh',
+            isSelected: false
+          },
+          {
+            id: 'C',
+            isCorrect: true,
+            answer: 'render',
+            isSelected: false
+          },
+          {
+            id: 'D',
+            isCorrect: false,
+            answer: 'constructor',
             isSelected: false
           },
         ]
       }
+
+
+
+
+
 
     ]
   },
