9755d144e11c9a2d0625f6a890e9078b6a5e698e
Paul Kirchhoff <paulkirchhoffbln@gmail.com>
2024-04-13 22:39:47 +0200

    create separate store for timer

---
+++ src/domain/exam/store/ExamTimerStore.ts
@@ -0,0 +1,15 @@
+import {atom, RecoilState} from "recoil";
+import {recoilPersist} from "recoil-persist";
+
+const {persistAtom} = recoilPersist({key: 'examTimer'});
+
+interface ExamTimerState {
+  timer: number
+}
+export const examTimerState: RecoilState<ExamTimerState> = atom({
+  key: 'ExamTimer',
+  default: {
+    timer: 0
+  } as ExamTimerState,
+  effects_UNSTABLE: [persistAtom]
+})
