408525f8ccff10ef41406513dff1712e2e7c85ad
Paul Kirchhoff <paulkirchhoffbln@gmail.com>
2024-04-16 13:50:35 +0200

    update ResultScreen

--- src/pages/ExamResult.tsx
+++ src/pages/ExamResult.tsx
@@ -3,34 +3,48 @@ import {useRecoilValue} from "recoil";
 import {activeExamState} from "../domain/exam/store/ActiveExamStore";
 import {useEffect, useState} from "react";
 import {Exam, Exams} from "../domain/exam/types/Exams";
+import {SelectedAnswer, selectedAnswersState} from "../domain/exam/store/SelectedAnswersStore";
+import {isEqual} from 'lodash';
+import {getCorrectAnswersFromExam, getSelectedAnswers} from "../domain/result/services/ResultService";
 
 export default function ExamResult() {
 
   const activeExam = useRecoilValue(activeExamState);
+  const selectedAnswers: SelectedAnswer[] = useRecoilValue(selectedAnswersState);
+
   const [examPercentResult, setExamPercentResult] = useState(0);
+  const [correctAnswersCount, setCorrectAnswersCount] = useState(0);
+  const [currentExam, setCurrentExam] = useState<Exam | null>(null);
+  const [successExam, setSuccessExam] = useState(false);
+
 
   useEffect(() => {
     const exam: Exam | undefined = Exams.find((e: Exam) => e.id === activeExam.examId);
-    if(exam) {
-      const map = exam.questions.map((q) => q.answers.filter((a) => a.isCorrect).map((a) => a.id));
-      console.log("Map,", map)
-      // const givenAnswers = activeExam.questionStates?.map((qs) => qs.givenAnswerIds);
-      // console.log("Given Answers", givenAnswers);
-      // map.map((a) => )
-
-      // activeExam.questionStates?.map((qs: QuestionState, index: number) => {
-      //   const correctAnswers: string[] = exam.questions[index].answers.filter((a: Answer) => a.isCorrect).map((a: Answer) => a.id);
-      //   const booleans = qs.givenAnswerIds.map((a) => correctAnswers.includes(a));
-      //   console.log("boolean", booleans)
-
-        // correctAnswers.forEach((cA) => ).includes(qs.givenAnswerIds)
-      // })
+    if (exam) {
+      setCurrentExam(exam);
+      calculateSuccess(exam);
     }
-  }, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [correctAnswersCount]);
+
+  const calculateSuccess = (exam: Exam) => {
+    const correctAnswers: SelectedAnswer[] = getCorrectAnswersFromExam(exam);
+    const answers: SelectedAnswer[] = getSelectedAnswers(selectedAnswers);
+    setCorrectAnswersCount(correctAnswers.map((ca, index) => (isEqual(ca, answers[index]))).filter((r) => r).length);
+    setExamPercentResult((correctAnswersCount / exam.questions.length) * 100);
+    setSuccessExam(exam.passingScore <= correctAnswersCount);
+  }
 
   return (
-      <Flex style={{height: '90vh'}}>
-        <h1>Exam result</h1>
+      <Flex vertical={true} justify={"left"} align={"flex-start"} style={{height: '90vh', width: '100%'}}>
+        <h3>Certification result: {currentExam?.title}</h3>
+        <Flex style={{width: '100%'}} justify={"space-between"}>
+          {successExam ?
+              <h4 style={{color: 'green'}}>Congratulations! You have passed the exam.</h4> :
+              <h4 style={{color: 'red'}}>Unfortunately, it wasn't enough today. Just try again</h4>
+          }
+          <h4>Correct Questions {correctAnswersCount} / {currentExam?.questions.length}</h4>
+        </Flex>
       </Flex>
   )
 }
