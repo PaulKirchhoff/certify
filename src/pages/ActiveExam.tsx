2c5d4c5e56bb490e92b8995856087788b204e63e
Paul Kirchhoff <paulkirchhoffbln@gmail.com>
2024-04-18 14:27:22 +0200

    remove unnecessary state access

--- src/pages/ActiveExam.tsx
+++ src/pages/ActiveExam.tsx
@@ -4,8 +4,6 @@ import {useLoaderData} from "react-router-dom";
 import ExamNavigationButtonPanel from "../domain/exam/components/ExamNavigationButtonPanel";
 import QuestionCard from "../domain/exam/components/QuestionCard";
 import Timer from "../domain/exam/components/Timer";
-import {useRecoilValue} from "recoil";
-import {ActiveExamState, activeExamState} from "../domain/exam/store/ActiveExamStore";
 
 export async function loader({params}: any) {
   const exam: Exam = Exams.filter((e: Exam) => e.id === params.examId)[0];
@@ -14,11 +12,10 @@ export async function loader({params}: any) {
 export default function ActiveExam() {
 
   const {exam} = useLoaderData() as {exam: Exam};
-  const activeExam: ActiveExamState = useRecoilValue<ActiveExamState>(activeExamState);
 
   return (
       <Flex vertical={true} justify={"space-between"} align={"center"} style={{height: '85vh'}}>
-        <QuestionCard question={activeExam.questions[activeExam.currentQuestionIndex]}/>
+        <QuestionCard />
         <ExamNavigationButtonPanel>
           <Timer time={exam.time} />
         </ExamNavigationButtonPanel>
