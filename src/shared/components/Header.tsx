2f238383819f425b87d8b2a5b17fed1f708ad6f2
Paul Kirchhoff <paulkirchhoffbln@gmail.com>
2024-04-19 13:49:12 +0200

    add condition and replace back button when navigate to exam result page

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -1,6 +1,6 @@
 import {Flex} from "antd";
-import {FaArrowAltCircleLeft} from "react-icons/fa";
-import {Link, useLocation, useNavigate} from "react-router-dom";
+import {FaArrowAltCircleLeft, FaHome} from "react-icons/fa";
+import {useLocation, useNavigate} from "react-router-dom";
 import {FaBrain} from "react-icons/fa6";
 
 export default function Header() {
@@ -12,9 +12,17 @@ export default function Header() {
     navigate(-1);
   }
 
+  const goHome = () => {
+    navigate('/');
+  }
+
+  const checkPathname = (): boolean => {
+    return location.pathname !== '/exams/exam/result' && location.pathname !== '/';
+  }
+
   return (
       <Flex vertical={false} align={"center"} style={{borderBottomWidth: 1, borderBottomStyle: "solid", borderBottomColor: 'rgba(218,218,218,0.83)', marginBottom: 10}}>
-        {location.pathname !== '/' && <FaArrowAltCircleLeft size={30} color={'#414141'} onClick={navigateBack}/>}
+        {checkPathname() ? <FaArrowAltCircleLeft size={30} color={'#414141'} onClick={navigateBack}/> : <FaHome size={30} color={'#414141'} onClick={goHome} /> }
         <Flex vertical={true} flex={"auto"}>
           <Flex vertical={false} justify={"flex-end"} align={"center"}>
             <h2 style={{color: '#414141'}}>Certify&nbsp;&nbsp;</h2>
