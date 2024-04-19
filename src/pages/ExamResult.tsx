3855a9289170999eae3874168837ba7a497969dd
Paul Kirchhoff <paulkirchhoffbln@gmail.com>
2024-04-19 11:33:07 +0200

    update ExamResult Screen

--- src/pages/ExamResult.tsx
+++ src/pages/ExamResult.tsx
@@ -1,4 +1,4 @@
-import {Card, Flex} from "antd";
+import {Card, Divider, Flex} from "antd";
 import {useRecoilValue} from "recoil";
 import {activeExamState} from "../domain/exam/store/ActiveExamStore";
 import {useEffect, useState} from "react";
@@ -11,9 +11,12 @@ import {
   getQuestionsByCategory,
 } from "../domain/result/services/ResultService";
 import PieChartResult from "../domain/result/components/charts/PieChartResult";
-import TreeMapResult from "../domain/result/components/charts/TreeMapResult";
 import StackedBarChartResult from "../domain/result/components/charts/StackedBarChartResult";
-import {buildPieChartData, buildStackedBarChartData} from "../domain/result/services/ChartService";
+import {
+  buildPieChartData,
+  buildStackedBarChartData,
+} from "../domain/result/services/ChartService";
+import {StackedBarChartData} from "../domain/result/types/StackedBarChartData";
 
 export default function ExamResult() {
 
@@ -24,6 +27,7 @@ export default function ExamResult() {
   const [correctAnswersCount, setCorrectAnswersCount] = useState(0);
   const [successExam, setSuccessExam] = useState(false);
   const [pieChartData, setPieChartData] = useState<any[]>([]);
+  const [stackedBarChartData, setStackedBarChartData] = useState<StackedBarChartData[]>([]);
 
 
   useEffect(() => {
@@ -31,46 +35,40 @@ export default function ExamResult() {
     if (exam) {
       setCurrentExam(exam);
 
-      const activeExamQuestionsByCategory: any = getQuestionsByCategory(activeExam.questions);
       const examQuestions: any = getQuestionsByCategory(exam.questions)
       setPieChartData(buildPieChartData(examQuestions));
+      setStackedBarChartData(buildStackedBarChartData(exam.questions, activeExam.questions));
 
       const correctAnswersFromExam: SelectedAnswer[] = getCorrectAnswersFromExamQuestions(exam.questions);
       const correctAnswersFromActiveExam: SelectedAnswer[] = getCorrectAnswersFromActiveExamQuestions(activeExam.questions);
-
-
-      buildStackedBarChartData(examQuestions, activeExamQuestionsByCategory)
-
-
       calculateSuccess(exam, correctAnswersFromExam, correctAnswersFromActiveExam);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [correctAnswersCount]);
 
   const calculateSuccess = (exam: Exam, examAnswers: SelectedAnswer[], activeExamAnswers: SelectedAnswer[]) => {
-
     setCorrectAnswersCount(examAnswers.map((ca, index) => (isEqual(ca, activeExamAnswers[index]))).filter((r) => r).length);
     setExamPercentResult((correctAnswersCount / exam.questions.length) * 100);
     setSuccessExam(exam.passingScore <= correctAnswersCount);
   }
 
   return (
-      <div>
-        <Flex vertical={true} justify={"left"} align={"flex-start"} style={{height: '90vh', width: '100%'}}>
-          <h3>Certification result: {currentExam?.title}</h3>
-          <Flex style={{width: '100%'}} justify={"space-between"}>
-            {successExam ?
-                <h4 style={{color: 'green'}}>Congratulations! You have passed the exam.</h4> :
-                <h4 style={{color: 'red'}}>Unfortunately, it wasn't enough today. Just try again</h4>
-            }
-            <h4>Correct Questions {correctAnswersCount} / {currentExam?.questions.length}</h4>
-          </Flex>
-          <Flex justify={'flex-start'} align={"center"} style={{height: '80vh', maxWidth: '100%'}}>
-            {/*<Card title={'skmskm'} style={{margin: 10}}><TreeMapResult /></Card>*/}
-            <Card title={'kndkn'} style={{margin: 10}}><PieChartResult data={pieChartData}/></Card>
-            <Card title={'jj  '} style={{margin: 10}}><StackedBarChartResult /></Card>
-          </Flex>
+      <Flex vertical={true} justify={"left"} align={"flex-start"} style={{height: '90vh', width: '100%'}}>
+        <h3>Certification result: {currentExam?.title}</h3>
+        <Flex style={{width: '100%'}} justify={"space-between"}>
+          {successExam ?
+              <h4 style={{color: 'green'}}>Congratulations! You have passed the exam.</h4> :
+              <h4 style={{color: 'red'}}>Unfortunately, it wasn't enough today. Just try again</h4>
+          }
+          <h4>Correct Questions {correctAnswersCount} / {currentExam?.questions.length}</h4>
         </Flex>
-      </div>
+        <Divider/>
+        <Flex justify={'center'} align={"center"} style={{width: '100%'}}>
+          {/*<Card title={'skmskm'} style={{margin: 10}}><TreeMapResult /></Card>*/}
+          <Card title={'Categories'} style={{margin: 10, width: '50%'}}><PieChartResult data={pieChartData}/></Card>
+          <Card title={'Question results'} style={{margin: 10, width: '50%'}}><StackedBarChartResult
+              data={stackedBarChartData}/></Card>
+        </Flex>
+      </Flex>
   )
 }
